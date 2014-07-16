0f7b106366f86325e34065b4b7346fb09f0ed8ff
ictxiangxin <ictxiangxin@gmail.com>
2014-07-16 18:49:49 +0800

    use array on one dimension

--- hypermatrix.h
+++ hypermatrix.h
@@ -50,7 +50,6 @@ namespace hm
             sparse<VALUE_T> *sl; // sparse list
             size_t size;
             size_t alloc_size;
-            size_t next;
             VALUE_T v;
 
             sparselist()
@@ -59,7 +58,6 @@ namespace hm
                 this->size = 0;
                 this->alloc_size = 0;
                 this->sl = NULL;
-                this->next = -1;
             }
 
             ~sparselist()
@@ -67,13 +65,12 @@ namespace hm
                 delete [] this->sl;
             }
 
-            void init(size_t size, size_t alloc_size, VALUE_T v)
+            void init(size_t size, size_t alloc_size, VALUE_T v, size_t x)
             {
-                this->x = 0;
+                this->x = x;
                 this->size = size;
                 this->alloc_size = alloc_size;
                 this->sl = new sparse<VALUE_T>[this->size];
-                this->next = -1;
                 for(size_t i = 0; i < this->size; i++)
                     init_sparse(this->sl[i], v);
             }
@@ -89,7 +86,7 @@ namespace hm
                             return this->sl[p].v;
                         else
                             p = this->sl[p].next;
-                    } while(p != 0);
+                    } while(p != 0 && this->sl[p].y <= index);
                 }
                 return this->v;
             }
@@ -132,7 +129,7 @@ namespace hm
                             end = p;
                             p = this->sl[p].next;
                         }
-                    } while(p != 0);
+                    } while(p != 0 && this->sl[p].y <= index);
                 }
                 if(this->v == v)
                     return false;
@@ -141,7 +138,7 @@ namespace hm
                     {
                         this->sl[i].y = index;
                         this->sl[i].v = v;
-                        this->sl[i].next = 0;
+                        this->sl[i].next = end == 0 ? 0 : this->sl[end].next;
                         this->sl[end].next = i;
                         return true;
                     }
@@ -156,7 +153,7 @@ namespace hm
                 this->sl = new_sl;
                 new_sl[new_i].y = index;
                 new_sl[new_i].v = v;
-                new_sl[new_i].next = 0;
+                new_sl[new_i].next = new_sl[end].next;
                 new_sl[end].next = new_i;
                 return true;
             }
@@ -171,7 +168,6 @@ namespace hm
                 this->x = spl.x;
                 this->size = spl.size;
                 this->alloc_size = spl.alloc_size;
-                this->next = spl.next;
                 for(size_t i = 0; i < spl.size; i++)
                     this->sl[i] = spl.sl[i];
             }
@@ -183,18 +179,16 @@ namespace hm
         private:
             sparselist<VALUE_T> *sb;
             size_t size;
-            size_t alloc_size;
             VALUE_T v;
 
         public:
-            sparseblock(size_t size, size_t alloc_size, VALUE_T v)
+            sparseblock(size_t block_size, size_t size, size_t alloc_size, VALUE_T v)
             {
-                this->size = size;
-                this->alloc_size = alloc_size;
+                this->size = block_size;
                 this->v = v;
                 this->sb = new sparselist<VALUE_T>[this->size];
                 for(size_t i = 0; i < this->size; i++)
-                    this->sb[i].init(this->size, this->alloc_size, this->v);
+                    this->sb[i].init(size, alloc_size, this->v, i);
             }
 
             ~sparseblock()
@@ -204,79 +198,28 @@ namespace hm
 
             VALUE_T get_value(size_t index, size_t jndex)
             {
-                size_t p = 0;
-                if(this->sb[p].next != -1)
-                {
-                    do
-                    {
-                        if(this->sb[p].x == index)
-                            return this->sb[p].get_value(jndex);
-                        else
-                            p = this->sb[p].next;
-                    } while(p != 0);
-                }
-                return this->v;
+                return this->sb[index].get_value(jndex);
             }
 
             bool set_value(size_t index, size_t jndex, VALUE_T v)
             {
-                size_t p = 0;
-                size_t end = p;
-                if(this->sb[p].next != -1)
-                {
-                    do
-                    {
-                        if(this->sb[p].x == index)
-                            return this->sb[p].set_value(jndex, v);
-                        else
-                        {
-                            end = p;
-                            p = this->sb[p].next;
-                        }
-                    } while(p != 0);
-                }
-                if(this->v == v)
-                {
-                    return false;
-                }
-                for(size_t i = 0; i < this->size; i++)
-                    if(this->sb[i].next == -1)
-                    {
-                        this->sb[i].x = index;
-                        this->sb[i].set_value(jndex, v);
-                        this->sb[i].next = 0;
-                        this->sb[end].next = i;
-                        return true;
-                }
-                sparselist<VALUE_T> *new_sb = new sparselist<VALUE_T>[this->size + this->alloc_size];
-                for(size_t i = 0; i < this->size; i++)
-                    new_sb[i] = this->sb[i];
-                for(size_t i = this->size; i < this->size + this->alloc_size; i++)
-                    new_sb[i].init(this->size, this->alloc_size, this->v);
-                size_t new_i = this->size;
-                this->size += this->alloc_size;
-                delete [] this->sb;
-                this->sb = new_sb;
-                new_sb[new_i].x = index;
-                new_sb[new_i].set_value(jndex, v);
-                new_sb[new_i].next = 0;
-                new_sb[end].next = new_i;
-                return true;
+                return this->sb[index].set_value(jndex, v);
             }
 
             void fill_normal(VALUE_T *array, size_t m)
             {
-                size_t px = 0;
-                do
+                for(size_t i = 0; i < this->size; i++)
                 {
-                    size_t py = 0;
-                    do
+                    size_t p = 0;
+                    if(this->sb[i].sl[p].next != -1)
                     {
-                        array[this->sb[px].x * m + this->sb[px].sl[py].y] = this->sb[px].sl[py].v;
-                        py = this->sb[px].sl[py].next;
-                    } while(py != 0);
-                    px = this->sb[px].next;
-                } while(px != 0);
+                        do
+                        {
+                            array[this->sb[i].x * m + this->sb[i].sl[p].y] = this->sb[i].sl[p].v;
+                            p = this->sb[i].sl[p].next;
+                        } while(p != 0);
+                    }
+                }
             }
     };
 
@@ -320,7 +263,7 @@ namespace hm
         n_size = n_size < 0x400 ? n_size : 0x400;
         m_size = m_size < 0x400 ? m_size : 0x400;
         debug("Final size: (%d, %d)\n", n_size, m_size);
-        this->_sparse = new sparseblock<VALUE_T>(n_size, m_size, v);
+        this->_sparse = new sparseblock<VALUE_T>(n, m_size, m_size, v);
         this->_normal = 0;
     }
 
@@ -352,7 +295,6 @@ namespace hm
         }
         if(this->status == STATUS_SPARSE)
         {
-            debug("status == STATUS_SPARSE\n");
             if(this->_sparse->set_value(index, jndex, v))
             {
                 if(this->v == v)
@@ -360,7 +302,6 @@ namespace hm
                 else
                     this->c++;
             }
-            debug("If it is too dense, c = %d.\n", this->c);
             if(this->c > this->size >> 2)
             {
                 debug("Matrix too dense, tranform to normal matrix.\n");
@@ -377,7 +318,6 @@ namespace hm
         }
         if(this->status == STATUS_NORMAL)
         {
-            debug("status == STATUS_NORMAL\n");
             size_t offset = index * this->m + jndex;
             if(this->_normal[offset] == this->v)
             {
@@ -390,15 +330,14 @@ namespace hm
                     this->c--;
             }
             this->_normal[offset] = v;
-            debug("If it is too sparse, c = %d.\n", this->c);
             if(this->c < this->size >> 4)
             {
                 debug("Matrix too sparse, tranform to sparse matrix.\n");
-                size_t n_size = n >> 2;
-                size_t m_size = m >> 2;
+                size_t n_size = this->n >> 2;
+                size_t m_size = this->m >> 2;
                 n_size = n_size ? n_size : 1;
                 m_size = m_size ? m_size : 1;
-                this->_sparse = new sparseblock<VALUE_T>(n_size, m_size, this->v);
+                this->_sparse = new sparseblock<VALUE_T>(this->n, m_size, m_size, this->v);
                 for(size_t i = 0; i < this->n; i++)
                     for(size_t j = 0; j < this->m; j++)
                     {
